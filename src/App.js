72457ce7c40ecfc442ae1e4738106fab133e0374
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-01-26 12:26:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const name = "Mert";
+const surname = "Ozturk";
+const isLoggedIn = true;
+
+const App = () => {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+      <h1>{isLoggedIn && `benim adım ${name},soyadım ${surname}`}</h1>
+      {!isLoggedIn && 'Giriş yapılmadı'}
+    </>
+  )
 }
 
-export default App;
+export default App
