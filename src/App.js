66d4aba7b10719f5fd2121e602bd02a08c196299
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-01-26 12:54:12 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import User from './components/User';
 
 
 const App = () => {
+
+  const friends = [
+  {
+    id:1,
+    name:"Ayşe",
+  },
+  {
+    id:2,
+    name:"Fatma",
+  },
+  {
+    id:3,
+    name:"Hayriye"
+  }
   
+  ];
 
   return (
     <>
-      <User name="Mert" isLoggedIn={true}/>
+
+      <User 
+      name="Mert" 
+      isLoggedIn={true} 
+      friends={friends}/>
      </>
   )
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const User = ({name,isLoggedIn}) => {
+const User = ({name,isLoggedIn,friends}) => {
   return (
     <h1>
         {isLoggedIn ? `Adım: ${name}`: 'Giriş Yapılmadı'}
-      
+      {
+        friends.map((friend,index)=> {
+            return <div key={friend.id}>{friend.name}</div>
+        }
+            
+        )
+      }
     </h1>
   )
 }
